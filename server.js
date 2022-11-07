2f11e828d4426715378f9dd06f390035603e8a32
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 20:14:49 +0200

    created two basic routes for the app with relevant responses

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
   console.log('here')
-  res.render('index', {text: 'world OF'})
+  res.render('index', {text: 'world'})
+})
+
+app.get('/users', (req, res)=>{
+  res.send('Users List')
+})
+
+app.get('/users/new', (req, res)=>{
+  res.send('Users New Form')
 })
 
 app.listen(3000)

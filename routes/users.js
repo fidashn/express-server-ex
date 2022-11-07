973cdb0576f382b1ae71044cd67243a8dc866ec4
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 20:23:26 +0200

    due to the nesting procedure, the router already goes to the users by deafault, which means the word users will automatically be included in our routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/users', (req, res)=>{
+router.get('/', (req, res)=>{
   res.send('Users List')
 })
 
-router.get('/users/new', (req, res)=>{
+router.get('/new', (req, res)=>{
   res.send('Users New Form')
 })

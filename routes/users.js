7c3c9696ed64919ad03d69c78e0c616b44a3a91f
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 20:55:03 +0200

    created update & delete routes

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,16 @@ router.get('/:id', (req, res)=>{
   res.send(`Get User With Id ${req.params.id}`)
 })
 
+router.put('/:id', (req, res)=>{
+  req.params.id
+  res.send(`Update User With Id ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res)=>{
+  req.params.id
+  res.send(`Delete User With Id ${req.params.id}`)
+})
+
 
 
 module.exports = router

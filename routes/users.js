ea84de8a3f876c05f904db2671a0a9ab53f43da4
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 21:32:57 +0200

    moved the middleware function to the users file

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(logger)
+
 router.get('/', (req, res)=>{
   res.send('Users List')
 })
@@ -50,6 +52,11 @@ router.post('/', (req, res)=>{
     req.user = users[id]
     next()
   })
+
+  function logger(req, res, next){
+    console.log(req.originalUrl)
+    next()
+  }
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(logger)
+
 
 app.get('/', (req, res)=>{
   console.log('here')
@@ -13,9 +13,6 @@ const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
 
-function logger(req, res, next){
-  console.log(req.originalUrl)
-  next()
-}
+
 
 app.listen(3000)

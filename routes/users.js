710f32ed1a4373f9de6de0c6f7787df1f51ce774
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 20:21:28 +0200

    nested the router app inside of the basic router of the original app

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-app.get('/users', (req, res)=>{
+router.get('/users', (req, res)=>{
   res.send('Users List')
 })
 
-app.get('/users/new', (req, res)=>{
+router.get('/users/new', (req, res)=>{
   res.send('Users New Form')
 })

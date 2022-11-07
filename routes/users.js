67367007ef12855472448e2758ee95cebd83d13f
fidashn <87206839+fidashn@users.noreply.github.com>
2022-11-07 20:58:21 +0200

    rewrote the coding method

--- routes/users.js
+++ routes/users.js
@@ -13,20 +13,34 @@ router.post('/', (req, res)=>{
   res.send('Create User')
 })
 
-router.get('/:id', (req, res)=>{
-  req.params.id
-  res.send(`Get User With Id ${req.params.id}`)
-})
 
-router.put('/:id', (req, res)=>{
-  req.params.id
-  res.send(`Update User With Id ${req.params.id}`)
-})
+// *********simple way*********
+// router.get('/:id', (req, res)=>{
+//   req.params.id
+//   res.send(`Get User With Id ${req.params.id}`)
+// })
 
-router.delete('/:id', (req, res)=>{
-  req.params.id
-  res.send(`Delete User With Id ${req.params.id}`)
-})
+// router.put('/:id', (req, res)=>{
+//   req.params.id
+//   res.send(`Update User With Id ${req.params.id}`)
+// })
+
+// router.delete('/:id', (req, res)=>{
+//   req.params.id
+//   res.send(`Delete User With Id ${req.params.id}`)
+// })
+
+//********much better & queue!  */
+  router.route('/:id').get('/:id', (req, res)=>{
+    req.params.id
+    res.send(`Get User With Id ${req.params.id}`)
+  }).put('/:id', (req, res)=>{
+      req.params.id
+      res.send(`Update User With Id ${req.params.id}`)
+    }).delete('/:id', (req, res)=>{
+        req.params.id
+        res.send(`Delete User With Id ${req.params.id}`)
+      })
 
 
 
